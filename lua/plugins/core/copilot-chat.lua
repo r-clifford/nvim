7e24f1c8f9c92909663fe39420e762277dd2a2c6
r-clifford <r-clifford@example.com>
2024-08-18 13:11:48 -0400

    Re-enable neotree, copilot

---
+++ lua/plugins/core/copilot-chat.lua
@@ -0,0 +1,1 @@
+../ai/copilot/copilot-chat.lua
--- lua/plugins/core/disabled.lua
+++ lua/plugins/core/disabled.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
+    enabled = true,
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
