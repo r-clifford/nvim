fd76bd5ae4fdbe634c74c90433ba4e0870fc397a
r-clifford <r-clifford@example.com>
2024-12-13 21:10:51 -0500

    Add binding for removing \r on windows files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("n", "<leader>cw", ":%s/\\r//g<cr>", { noremap = true, silent = true, desc = "Remove ^M" })
